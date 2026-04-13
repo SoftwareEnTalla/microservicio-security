d464980fb342d65781c549f34904b5974e8669e9
SoftwarEnTalla <softwarentalla@gmail.com>
2026-04-12 20:25:54 -0700

    Commit automático previo a pull --rebase (script)

---
+++ src/modules/login/graphql/login.resolver.ts
@@ -0,0 +1,455 @@
+/*
+ * Copyright (c) 2026 SoftwarEnTalla
+ * Licencia: MIT
+ * Contacto: softwarentalla@gmail.com
+ * CEOs: 
+ *       Persy Morell Guerra      Email: pmorellpersi@gmail.com  Phone : +53-5336-4654 Linkedin: https://www.linkedin.com/in/persy-morell-guerra-288943357/
+ *       Dailyn García Domínguez  Email: dailyngd@gmail.com      Phone : +53-5432-0312 Linkedin: https://www.linkedin.com/in/dailyn-dominguez-3150799b/
+ *
+ * CTO: Persy Morell Guerra
+ * COO: Dailyn García Domínguez and Persy Morell Guerra
+ * CFO: Dailyn García Domínguez and Persy Morell Guerra
+ *
+ * Repositories: 
+ *               https://github.com/SoftwareEnTalla 
+ *
+ *               https://github.com/apokaliptolesamale?tab=repositories
+ *
+ *
+ * Social Networks:
+ *
+ *              https://x.com/SoftwarEnTalla
+ *
+ *              https://www.facebook.com/profile.php?id=61572625716568
+ *
+ *              https://www.instagram.com/softwarentalla/
+ *              
+ *
+ *
+ */
+
+
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
+
+//Definición de entidades
+import { Login } from "../entities/login.entity";
+
+//Definición de comandos
+import {
+  CreateLoginCommand,
+  UpdateLoginCommand,
+  DeleteLoginCommand,
+} from "../commands/exporting.command";
+
+import { CommandBus } from "@nestjs/cqrs";
+import { LoginQueryService } from "../services/loginquery.service";
+
+
+import { LoginResponse, LoginsResponse } from "../types/login.types";
+import { FindManyOptions } from "typeorm";
+import { PaginationArgs } from "src/common/dto/args/pagination.args";
+import { fromObject } from "src/utils/functions";
+
+//Logger
+import { LogExecutionTime } from "src/common/logger/loggers.functions";
+import { LoggerClient } from "src/common/logger/logger.client";
+import { logger } from '@core/logs/logger';
+
+import { v4 as uuidv4 } from "uuid";
+
+//Definición de tdos
+import { UpdateLoginDto, 
+CreateOrUpdateLoginDto, 
+LoginValueInput, 
+LoginDto, 
+CreateLoginDto } from "../dtos/all-dto";
+ 
+
+//@UseGuards(JwtGraphQlAuthGuard)
+@Resolver(() => Login)
+export class LoginResolver {
+
+   //Constructor del resolver de Login
+  constructor(
+    private readonly service: LoginQueryService,
+    private readonly commandBus: CommandBus
+  ) {}
+
+  @LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  // Mutaciones
+  @Mutation(() => LoginResponse<Login>)
+  async createLogin(
+    @Args("input", { type: () => CreateLoginDto }) input: CreateLoginDto
+  ): Promise<LoginResponse<Login>> {
+    return this.commandBus.execute(new CreateLoginCommand(input));
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Mutation(() => LoginResponse<Login>)
+  async updateLogin(
+    @Args("id", { type: () => String }) id: string,
+    @Args("input") input: UpdateLoginDto
+  ): Promise<LoginResponse<Login>> {
+    const payLoad = input;
+    return this.commandBus.execute(
+      new UpdateLoginCommand(payLoad, {
+        instance: payLoad,
+        metadata: {
+          initiatedBy: payLoad.createdBy || 'system',
+          correlationId: payLoad.id,
+        },
+      })
+    );
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Mutation(() => LoginResponse<Login>)
+  async createOrUpdateLogin(
+    @Args("data", { type: () => CreateOrUpdateLoginDto })
+    data: CreateOrUpdateLoginDto
+  ): Promise<LoginResponse<Login>> {
+    if (data.id) {
+      const existingLogin = await this.service.findById(data.id);
+      if (existingLogin) {
+        return this.commandBus.execute(
+          new UpdateLoginCommand(data, {
+            instance: data,
+            metadata: {
+              initiatedBy:
+                (data.input as CreateLoginDto | UpdateLoginDto).createdBy ||
+                'system',
+              correlationId: data.id,
+            },
+          })
+        );
+      }
+    }
+    return this.commandBus.execute(
+      new CreateLoginCommand(data, {
+        instance: data,
+        metadata: {
+          initiatedBy:
+            (data.input as CreateLoginDto | UpdateLoginDto).createdBy ||
+            'system',
+          correlationId: data.id || uuidv4(),
+        },
+      })
+    );
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Mutation(() => Boolean)
+  async deleteLogin(
+    @Args("id", { type: () => String }) id: string
+  ): Promise<boolean> {
+    return this.commandBus.execute(new DeleteLoginCommand(id));
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  // Queries
+  @Query(() => LoginsResponse<Login>)
+  async logins(
+    options?: FindManyOptions<Login>,
+    paginationArgs?: PaginationArgs
+  ): Promise<LoginsResponse<Login>> {
+    return this.service.findAll(options, paginationArgs);
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginsResponse<Login>)
+  async login(
+    @Args("id", { type: () => String }) id: string
+  ): Promise<LoginResponse<Login>> {
+    return this.service.findById(id);
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginsResponse<Login>)
+  async loginsByField(
+    @Args("field", { type: () => String }) field: string,
+    @Args("value", { type: () => LoginValueInput }) value: LoginValueInput,
+    @Args("page", { type: () => Number, defaultValue: 1 }) page: number,
+    @Args("limit", { type: () => Number, defaultValue: 10 }) limit: number
+  ): Promise<LoginsResponse<Login>> {
+    return this.service.findByField(
+      field,
+      value,
+      fromObject.call(PaginationArgs, { page: page, limit: limit })
+    );
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginsResponse<Login>)
+  async loginsWithPagination(
+    @Args("page", { type: () => Number, defaultValue: 1 }) page: number,
+    @Args("limit", { type: () => Number, defaultValue: 10 }) limit: number
+  ): Promise<LoginsResponse<Login>> {
+    const paginationArgs = fromObject.call(PaginationArgs, {
+      page: page,
+      limit: limit,
+    });
+    return this.service.findWithPagination({}, paginationArgs);
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => Number)
+  async totalLogins(): Promise<number> {
+    return this.service.count();
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginsResponse<Login>)
+  async searchLogins(
+    @Args("where", { type: () => LoginDto, nullable: false })
+    where: Record<string, any>
+  ): Promise<LoginsResponse<Login>> {
+    const logins = await this.service.findAndCount(where);
+    return logins;
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginResponse<Login>, { nullable: true })
+  async findOneLogin(
+    @Args("where", { type: () => LoginDto, nullable: false })
+    where: Record<string, any>
+  ): Promise<LoginResponse<Login>> {
+    return this.service.findOne(where);
+  }
+
+
+@LogExecutionTime({
+    layer: 'resolver',
+    callback: async (logData, client) => {
+      // Puedes usar el cliente proporcionado o ignorarlo y usar otro
+      try{
+        logger.info('Información del cliente y datos a enviar:',[logData,client]);
+        return await client.send(logData);
+      }
+      catch(error){
+        logger.info('Ha ocurrido un error al enviar la traza de log: ', logData);
+        logger.info('ERROR-LOG: ', error);
+        throw error;
+      }
+    },
+    client: LoggerClient.getInstance()
+      .registerClient(LoginResolver.name)
+
+      .get(LoginResolver.name),
+    })
+  @Query(() => LoginResponse<Login>)
+  async findOneLoginOrFail(
+    @Args("where", { type: () => LoginDto, nullable: false })
+    where: Record<string, any>
+  ): Promise<LoginResponse<Login> | Error> {
+    return this.service.findOneOrFail(where);
+  }
+}
+
