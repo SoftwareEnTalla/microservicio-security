dd03ccf54128ad5dc0b83d3d8f2672325782eb07
SoftwarEnTalla <softwarentalla@gmail.com>
2026-04-13 01:10:52 -0700

    Commit automático previo a pull --rebase (script)

--- src/modules/security-merchant/controllers/securitymerchantquery.controller.ts
+++ src/modules/security-merchant/controllers/securitymerchantquery.controller.ts
@@ -36,14 +36,16 @@ import {
   Param,
   NotFoundException,
   Logger,
+  UseGuards,
 } from "@nestjs/common";
 import { SecurityMerchantQueryService } from "../services/securitymerchantquery.service";
 import { FindManyOptions } from "typeorm";
-import { ApiTags, ApiOperation, ApiResponse, ApiQuery, ApiParam } from "@nestjs/swagger";
+import { ApiTags, ApiOperation, ApiResponse, ApiQuery, ApiParam, ApiBearerAuth, ApiUnauthorizedResponse } from "@nestjs/swagger";
 import { LogExecutionTime } from "src/common/logger/loggers.functions";
 import { SecurityMerchantResponse, SecurityMerchantsResponse } from "../types/securitymerchant.types";
 import { LoggerClient } from "src/common/logger/logger.client";
 import { SecurityMerchant } from "../entities/security-merchant.entity";
+import { SecurityMerchantAuthGuard } from "../guards/securitymerchantauthguard.guard";
 import { PaginationArgs } from "src/common/dto/args/pagination.args";
 import { OrderBy, valueOfOrderBy } from "src/common/types/common.types";
 import { Helper } from "src/common/helpers/helpers";
@@ -52,6 +54,9 @@ import { SecurityMerchantDto } from "../dtos/all-dto";
 import { logger } from '@core/logs/logger';
 
 @ApiTags("SecurityMerchant Query")
+@UseGuards(SecurityMerchantAuthGuard)
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ status: 401, description: "Autenticación requerida." })
 @Controller("securitymerchants/query")
 export class SecurityMerchantQueryController {
   #logger = new Logger(SecurityMerchantQueryController.name);
